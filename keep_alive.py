c9676255c6bee6fccfb2ad828f4d1b913a736a74
melforkani <72602320+melforkani@users.noreply.github.com>
2025-06-25 18:24:30 +0100

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,5 +1,5 @@
 import time
 
-print("Processus principal lancé (dummy).")
+# Ce script tourne en arrière-plan pour que Railway garde le projet actif.
 while True:
-    time.sleep(3600)
+    time.sleep(3600)  # Attente d'une heure avant de refaire une boucle
